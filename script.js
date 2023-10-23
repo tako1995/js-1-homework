4413049978738f7c433fd3bb16716a64914825c7
tako1995 <141060940+tako1995@users.noreply.github.com>
2023-10-24 02:55:16 +0400

    homework

---
+++ script.js
@@ -0,0 +1,46 @@
+
+//  1 homework
+
+let number = 100;
+
+if (number < 50) {
+
+    console.log(">50");
+}
+
+else if (number > 20) {
+    console.log(">20");
+}
+
+else {
+    console.log("error");
+}
+
+
+
+//  2 homework
+
+
+let username = "მარიამ";
+
+if (username == "მარიამ") {
+    console.log("true");
+}
+
+else { console.log("false"); }
+
+
+//  3 homework
+
+
+let FirstName = "მარიამ";
+
+switch (FirstName) {
+    case "მარიამ":
+        console.log("true");
+        break;
+
+    default:
+        console.log("false");
+       
+}
